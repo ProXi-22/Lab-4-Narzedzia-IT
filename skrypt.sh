0b0b22a7c29ffec09c563e29d8a6d7fce4359199
ProXi-22 <michalopl15@gmail.com>
2024-07-05 21:54:22 +0200

    Dodanie help

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+function show_help {
+	echo "Dostępne opcje:"
+	echo "--date -d		Wyświetlanie daty"
+	echo "--logs -l [N]	Tworzenie N plików logx.txt (domyślnie 100)"
+	echo "--help -h		Pomoc"
+}
+
 function show_date {
 	date
 }
