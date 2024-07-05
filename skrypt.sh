f230ec8e1119ce2cb15501f9dc43573ae0b9cd95
ProXi-22 <michalopl15@gmail.com>
2024-07-05 22:01:51 +0200

    Dodanie funkcji logs

--- skrypt.sh
+++ skrypt.sh
@@ -10,3 +10,26 @@ function show_help {
 function show_date {
 	date
 }
+
+function create_logs {
+	num_files=${1:-100}
+	for i in $(seq 1 $num_files); do
+	echo "log${i}.txt, skrypt.sh, $(date)" > log${i}.txt
+	done
+}
+
+case "$1" in
+	--date|-d)
+        show_date
+        ;;
+    	--logs|-l)
+        create_logs $2
+        ;;
+    	--help|-h)
+        show_help
+        ;;
+*)
+        show_help
+        ;;
+esac
+
